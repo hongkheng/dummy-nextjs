feaa349c4e307e256511b5d366293f7471322790
yaphongkheng <hongkheng.yap@gmail.com>
2025-03-05 10:38:15 +0800

    fix: add logout call

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Header } from "@/components/ui/header";
 import { auth0 } from "@/lib/auth0";
 
@@ -10,18 +9,25 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header>
-        <Button variant="outline">login with auth0</Button>
+        <div>Mock External Host Portal</div>
       </Header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>Pretend X IDP</p>
+        {
+          !session && <>
+            <a href="/auth/login?screen_hint=signup">Sign up with auth0</a>
+            <a href="/auth/login">Log in auth0</a>
+          </>
+        }
 
-        <a href="/auth/login?screen_hint=signup">Sign up</a>
-        <a href="/auth/login">Log in</a>
-
-        {session && <div>{JSON.stringify(session)}</div>}
+        {session && <>
+          <div>{JSON.stringify(session)}</div>
+          <div>
+            <a href="/auth/logout">Logout with auth0</a>
+          </div>
+          </>}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
+        mock demo
       </footer>
     </div>
   );
