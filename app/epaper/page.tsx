88ddd3215bbb71c33582744c1f49003006b5bca9
HongKheng Yap <hongkheng@sph.com.sg>
2025-03-14 10:19:25 +0800

    feat: try ory login

--- app/epaper/page.tsx
+++ app/epaper/page.tsx
@@ -1,4 +1,42 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import { Configuration, FrontendApi, Identity, Session } from "@ory/client";
+import { edgeConfig } from "@ory/integrations/next";
+
+const ory = new FrontendApi(new Configuration(edgeConfig))
+
+// Returns either the email or the username depending on the user's Identity Schema
+const getUserName = (identity: Identity | undefined) =>
+  identity?.traits.email || identity?.traits.username || "user"
+
 export default function Page() {
+  const router = useRouter()
+  const [session, setSession] = useState<Session | undefined>()
+  const [logoutUrl, setLogoutUrl] = useState<string | undefined>()
+
+  useEffect(() => {
+    ory
+      .toSession()
+      .then(({ data }) => {
+        // User has a session!
+        setSession(data)
+        // Create a logout url
+        ory.createBrowserLogoutFlow().then(({ data }) => {
+          setLogoutUrl(data.logout_url)
+        })
+      })
+      .catch(() => {
+        // Redirect to login page
+        return router.push(edgeConfig.basePath + "/ui/login")
+      })
+  }, [router])
+
+  if (!session) {
+    // Still loading
+    return null
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header>
@@ -6,6 +44,13 @@ export default function Page() {
       </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h3>Login with Ory</h3>
+
+        <p>Hello, {getUserName(session.identity)}</p>
+          <div>
+            <p className="sm:p-20 font-[family-name:var(--font-geist-sans)">
+              <a href={logoutUrl}>Log out</a>
+            </p>
+          </div>        
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@ import { Header } from "@/components/ui/header";
 import { auth0 } from "@/lib/auth0";
 
 export default async function Home() {
-  const session = await auth0.getSession();
+  const auth0session = await auth0.getSession();
 
-  console.log("session", { session });
-
+  console.log("session", { auth0session });
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header>
@@ -13,14 +12,14 @@ export default async function Home() {
       </Header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {
-          !session && <>
+          !auth0session && <>
             <a href="/auth/login?screen_hint=signup">Sign up with auth0</a>
             <a href="/auth/login">Log in auth0</a>
           </>
         }
 
-        {session && <>
-          <div>{JSON.stringify(session)}</div>
+        {auth0session && <>
+          <div>{JSON.stringify(auth0session)}</div>
           <div>
             <a href="/auth/logout">Logout with auth0</a>
           </div>
