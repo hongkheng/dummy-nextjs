930ecebc91c680c43829fe00373e9769337b8dbc
HongKheng Yap <hongkheng@sph.com.sg>
2025-03-19 16:43:46 +0800

    fix: add return_to param

--- app/epaper/page.tsx
+++ app/epaper/page.tsx
@@ -8,48 +8,16 @@
 //   identity?.traits.email || identity?.traits.username || "user"
 
 export default function Page() {
-  // const router = useRouter()
-  // const [session, setSession] = useState<Session | undefined>()
-  // const [logoutUrl, setLogoutUrl] = useState<string | undefined>()
-
-  // useEffect(() => {
-  //   ory
-  //     .toSession()
-  //     .then(({ data }) => {
-  //       // User has a session!
-  //       setSession(data)
-  //       console.log("session", { data })
-  //       // Create a logout url
-  //       // ory.createBrowserLogoutFlow().then(({ data }) => {
-  //       //   setLogoutUrl(data.logout_url)
-  //       // })
-  //     })
-  //     .catch(() => {
-  //       // Redirect to login page
-
-  //       return
-  //       // return router.push(edgeConfig.basePath + "/ui/login")
-  //     })
-  // }, [router])
-
-  // if (!session) {
-  //   // Still loading
-  //   return null
-  // }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header>
         <h1>test dummy home page</h1>
       </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <a href="https://priceless-rhodes-xs818rwyhy.projects.oryapis.com/self-service/login/browser">login with Ory</a>
-
-        {/* <p>Hello, {getUserName(session.identity)}</p> */}
-          {/* <div>
-            <p className="sm:p-20 font-[family-name:var(--font-geist-sans)">
-              <a href={logoutUrl}>Log out</a>
-            </p>
-          </div>         */}
+        <div>
+          <a href="">Trigger custom auth flow</a>
+        </div>
+        <a href="https://priceless-rhodes-xs818rwyhy.projects.oryapis.com/self-service/login/browser?return_to=https://dummy-nextjs-xi.vercel.app/">Login with Ory Network</a>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
